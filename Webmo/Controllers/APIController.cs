421b3887eb356202aa9cf9bcd4125159c7774e14
Kuraara Forrest <kuraara.forrest@gmail.com>
2015-07-15 19:19:50 +0800

    CSS styling and layout stuff added/fixed.

--- Webmo/Controllers/APIController.cs
+++ Webmo/Controllers/APIController.cs
@@ -9,6 +9,9 @@ using System.Web.Mvc;
 using System.Xml;
 using System.IO;
 using System.Reflection;
+
+using MarkdownSharp;
+
 namespace Webmo.Controllers
 {
     public class APIController : Controller
@@ -22,6 +25,15 @@ namespace Webmo.Controllers
         {
 
             return View();
+        }
+
+        public JsonResult mdToHtml(string text)
+        {
+           
+            Markdown md = new Markdown();
+            string h = md.Transform(text);
+
+            return Json(new {Original=text, Result=h });
         }
     }
 }
--- Webmo/Extensions/Extensions.cs
+++ Webmo/Extensions/Extensions.cs
@@ -4,6 +4,7 @@ using System.Web.Mvc.Ajax;
 using System.Web.Mvc.Html;
 using Webmo.Data.Models;
 using MarkdownSharp;
+using System;
 
 namespace Webmo.Extensions
 {
@@ -13,6 +14,16 @@ namespace Webmo.Extensions
         {
             int n = a.Content.Length < 160 ? a.Content.Length : 160;
             return a.Content.Substring(0, n);
+        }
+
+        public static MvcHtmlString RawActionLink(this HtmlHelper htmlHelper, string rawHtml, string action, string controller, object routeValues, object htmlAttributes)
+        {
+            //string anchor = htmlHelper.ActionLink("##holder##", action, controller, routeValues, htmlAttributes).ToString();
+            //return MvcHtmlString.Create(anchor.Replace("##holder##", rawHtml));
+            /* Updated code to use a generated guid as from the suggestion of Phillip Haydon */
+            string holder = Guid.NewGuid().ToString();
+            string anchor = htmlHelper.ActionLink(holder, action, controller, routeValues, htmlAttributes).ToString();
+            return MvcHtmlString.Create(anchor.Replace(holder, rawHtml));
         }
 
         public static MvcHtmlString ProcessMarkDown(this Article a)
